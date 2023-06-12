e82e4c3aa098188d14997822b78f2c8a4c647c60
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-12 16:08:48 +0900

    feat: show count of books in lists

--- src/pages/UserBookShelf.js
+++ src/pages/UserBookShelf.js
@@ -19,7 +19,8 @@ const Book = (props) => (
 
 // eslint-disable-next-line import/no-anonymous-default-export
 function getlist(books_info){ 
-  return (
+  return ([
+    books_info.length,
     <div style={{display : "flex", overflowX: "auto"}}>
     {books_info.map((book_info) => (
     <Book
@@ -30,7 +31,7 @@ function getlist(books_info){
     />
   ))}
   </div>
-)}
+])}
 
 function trim(books_info, includeCount){
   let res = []
@@ -54,15 +55,19 @@ function get_likes_list(books_info){
 }
 
 const MyLibrary = () => {
+  let [recentlyAdded_count, recentlyAdded_list] = get_recentlyAdded_list(sample);
+  let [totalBook_count, totalBook_list] = getlist(sample);
+  let [likes_count, Likes_list] = get_likes_list(sample);
+
   return (
     <div>
       <h1>나의 서재</h1>      
-      <h2>최근 추가한 항목</h2>      
-      {get_recentlyAdded_list(sample)}
-      <h2>전체 보기</h2>      
-      {getlist(sample)}
-      <h2>좋아요</h2>
-      {get_likes_list(sample)}
+      <h2>최근 추가한 항목({recentlyAdded_count}개)</h2>      
+      {recentlyAdded_list}
+      <h2>전체 보기 (총 {totalBook_count}개)</h2>      
+      {totalBook_list}
+      <h2>좋아요({likes_count}개)</h2>
+      {Likes_list}
     </div>
   );
 };

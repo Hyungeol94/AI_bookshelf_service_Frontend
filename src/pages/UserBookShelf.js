67faeb9cad3299a894de2078fb2ef9819fdae7b2
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-12 14:14:59 +0900

    feat: create temporary variable Book

--- src/pages/UserBookShelf.js
+++ src/pages/UserBookShelf.js
@@ -2,12 +2,32 @@ import React from "react";
 import BookList from "../components/BookList";
 import sample from "../assets/sample_book.json";
 
+const Book = (props) => (
+  <div>
+    <div>
+      <img src={props.image} 
+      alt={props.booktitle} 
+      width={80} 
+      height={80}
+      style = {{marginRight : "10px"}}
+      />
+    </div>
+    <h5>{props.booktitle}</h5>
+  </div>
+);
+
+
 // eslint-disable-next-line import/no-anonymous-default-export
 function getlist(books_info){ 
   return (
-    <div style={{display : "flex"}}>
-    {books_info.map((book) => (
-    <BookList key={book.id} image={book.image} booktitle={book.booktitle}/>
+    <div style={{display : "flex", overflowX: "auto"}}>
+    {books_info.map((book_info) => (
+    <Book
+      key={book_info.id}
+      image={book_info.image}
+      booktitle={book_info.booktitle}
+      style = {{marginRight : "10px"}}
+    />
   ))}
   </div>
 )}

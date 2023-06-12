f0be4f76b205cf5345250a6e70ebfcc872c09bb5
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-12 16:07:50 +0900

    add navigator to UserBookShelf page

--- src/routes/UserBookShelf.js
+++ src/routes/UserBookShelf.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserBookShelfpage from "../pages/UserBookShelf";
+import Navigator from "../components/Navigator";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 class UserBookShelf extends React.Component {
@@ -16,6 +17,7 @@ class UserBookShelf extends React.Component {
   render() {
     return (
       <div>
+        <Navigator />
         {this.state.isLoading ? <h1>"Loading..."</h1> : <UserBookShelfpage />}
       </div>
     );

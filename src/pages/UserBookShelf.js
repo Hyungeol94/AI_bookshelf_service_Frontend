7bd76acbb0ba023ef1f6256f32b8d434559b100c
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-12 13:27:56 +0900

    refactor: rename get_trimmed_list to get_recentlyAdded_list

--- src/pages/UserBookShelf.js
+++ src/pages/UserBookShelf.js
@@ -12,10 +12,10 @@ function getlist(books_info){
   </div>
 )}
 
-function trim(books_info){
+function trim(books_info, includeCount){
   let res = []
   for (let i =0;i<books_info.length;i++){
-    if (books_info[i].id <= 3){
+    if (books_info[i].id <= includeCount){
       res.push(books_info[i])
     }
   }
@@ -23,9 +23,9 @@ function trim(books_info){
 }
 
 
-function get_trimmed_list(books_info){ 
+function get_recentlyAdded_list(books_info){ 
   const includeCount = 3;
-  let res = trim(books_info)
+  let res = trim(books_info, includeCount)
   return getlist(res)
 }
 
@@ -35,7 +35,7 @@ const MyLibrary = () => {
     <div>
       <h1>나의 서재</h1>      
       <h2>최근 추가한 항목</h2>      
-      {get_trimmed_list(sample)}
+      {get_recentlyAdded_list(sample)}
       <h2>전체 보기</h2>      
       {getlist(sample)}
     </div>

b52802838495a8ba37c6158ca06a672dc6eadbf8
kim-deokju <edaumedo1@naver.com>
2023-06-13 11:12:28 +0900

    updata search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,26 +2,38 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from "react";
 import "../styles/Home.css";
+import { Select, MenuItem } from "@mui/material";
 import { Link } from "react-router-dom";
-// import BookList from "../components/BookList";
+import BookList from "../components/BookList";
 import bookinfo_api from "../services/bookinfo_api";
+import sampleBookImg from "../assets/img/sample_book.png";
 // import sample from "../assets/sample_book.json";
 
 export default () => {
   const [data, setData] = useState(null);
-  const [searchvalue, setSearchvalue] = useState(null);
+  const [searchValue, setSearchValue] = useState(null);
+  const [pageSize, setPageSize] = useState(10);
 
   const onChange = (e) => {
-    console.log(data); //이벤트가 발생한 타겟의 요소를 출력
-    setSearchvalue(e.target.value);
-    console.log(searchvalue); //이벤트가 발생한 타겟의 Value를 출력
+    setSearchValue(e.target.value);
+  };
+
+  const onChangePageSize = (e) => {
+    setPageSize(e.target.value);
   };
 
   const onSearch = async () => {
-    const res = await bookinfo_api(searchvalue);
-    await setData(res);
-    console.log("data", data);
+    setData(await bookinfo_api(searchValue, pageSize));
+    // console.log("data", data[0].elements[0].elements[0].cdata);
   };
+
+  const handleOnKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onSearch(); // Enter 입력이 되면 클릭 이벤트 실행
+    }
+  };
+
+  let num = 1;
 
   return (
     <div id="search">
@@ -29,19 +41,49 @@ export default () => {
       <Link to="/bookshelf">
         <button>나의 서재</button>
       </Link>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={pageSize}
+        label="페이지수"
+        style={{ height: "2em" }}
+        onChange={onChangePageSize}
+      >
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+        <MenuItem value={30}>30</MenuItem>
+      </Select>
       <div>
-        <input id="search-item" onChange={onChange}></input>
+        <input
+          id="search-item"
+          onChange={onChange}
+          onKeyDown={handleOnKeyPress}
+        ></input>
         <button
           id="search-btn"
-          onClick={async () => await onSearch(searchvalue)}
+          onClick={async () => await onSearch(searchValue)}
         >
           검색
         </button>
       </div>
+      {data ? (
+        <div>
+          {data.map((book) => {
+            return (
+              <BookList
+                key={num++}
+                booktitle={book?.elements[0]?.elements[0]?.cdata}
+                image={sampleBookImg}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          <h3>검색결과가 없습니다</h3>
+        </div>
+      )}
       {/* <p>bookinfo</p> */}
-      {/* {data.map((book) => (
-        <BookList key={book.id} booktitle={book.booktitle} image={book.image} />
-      ))} */}
     </div>
   );
 };
--- src/services/bookinfo_api.js
+++ src/services/bookinfo_api.js
@@ -5,12 +5,14 @@ const BOOKINFO_APIKEY =
   "f2671199c0a54a75884db863acdb6287397dba388499b9f6e4a5dcb64f29d503";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (res) => {
+export default async (searchvalue, pageSize) => {
+  // console.log(searchvalue, pageSize);
   const bookdata = await axios
     .get(`https://nl.go.kr/NL/search/openApi/search.do`, {
       params: {
         key: BOOKINFO_APIKEY,
-        kwd: res,
+        kwd: searchvalue,
+        pageSize: pageSize || 10,
       },
     })
     .then((data) => JSON.parse(converter.xml2json(data.data)))

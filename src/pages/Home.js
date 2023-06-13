e85f60ca13f9ab6755167a45fcad4859f46d6adc
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-13 15:05:43 +0900

    fix typo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ let [totalBook_count, totalBook_list] = getlist(sample);
 export default () => {
   return (
     <div id="home">
-      <h1>Home 이다.</h1>   
+      <h1>Home</h1>   
       <h2>추천 도서</h2>      
       {totalBook_list}
       <h2>나의 서재(총 {totalBook_count}개)</h2>      

57f5c9a0e23b41d9774eee0969aaf96029965279
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-13 16:57:32 +0900

    refactor: modify Upload page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import "../styles/Upload.css";
 
 const Upload = () => {
   const [imgFile, setImgFile] = useState([]); // 이미지 배열
@@ -6,6 +7,10 @@ const Upload = () => {
 
   const imgUpload = () => {
     console.log(upload.current.files);
+    setImgFile((prev) => [...prev, URL.createObjectURL(upload.current.files[0])]);
+  };
+
+  const boximgUpload = () => {
     setImgFile((prev) => [...prev, URL.createObjectURL(upload.current.files[0])]);
   };
 
@@ -21,36 +26,48 @@ const Upload = () => {
 
   return (
     <>
-      <h1>서재 이미지 업로드</h1>
-      <input
-        type="file"
-        ref={upload}
-        multiple
-        onChange={imgUpload}
-        accept="image/*"
-        style={{
-          zIndex: '2',
-          marginBottom: '10px',
-          cursor: 'pointer',
-        }}
-      />
-      <h2>이미지 저장소</h2>
-      {imgFile.length === 0 ? (
-        <p>No images appended.</p>
-      ) : (
-        <div style={{ display: 'flex' }}>
-          {imgFile?.map((img, idx) => (
-            <div key={idx} style={{ margin: '20px', border: '1px solid black' }}>
-              <img
-                style={{ width: '200px', height: '200px' }}
-                src={img}
-                alt="img"
-              />
-            </div>
-          ))}
-        </div>
-      )}
-      <button onClick={handleUpload}>서재 목록 만들기</button>
+      <h1 className="head">책장 사진을 업로드해 주세요</h1>
+      <h5 className="explain">정면에서 책장 사진을 찍어 업로드해 주세요.</h5>
+      <h5 className="explain">인공지능이 책을 감지해 자동으로 내 서재를 만들어 줄 거에요.</h5>
+     
+       <div className="upload-box">
+        {imgFile.length === 0 ? (
+            <>
+            <p>책장 이미지를 업로드해 주세요.</p>
+            <input
+            type="file"
+            ref={upload}
+            multiple
+            onChange={boximgUpload}
+            accept="image/*"
+            value=""
+            />
+            </>
+        ) : (
+            <>
+                <div style={{ display: 'flex' }}>
+                {imgFile?.map((img, idx) => (
+                    <div key={idx} style={{ margin: '20px', border: '1px solid black' }}>
+                    <img
+                        style={{ width: '200px', height: '200px' }}
+                        src={img}
+                        alt="img"
+                    />
+                    </div>
+                ))}
+                </div>
+                <input
+                type="file"
+                ref={upload}
+                multiple
+                onChange={boximgUpload}
+                accept="image/*"                
+            />
+            </>
+            
+        )}
+      </div>
+      <button onClick={handleUpload} className="continueButton">계속하기</button>
     </>
   );
 };

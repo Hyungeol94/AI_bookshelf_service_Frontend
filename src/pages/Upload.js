9751f404011540526d8bbaccdadcb09d25f12945
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-13 15:42:44 +0900

    feat: handle exception and revise button expression

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,19 +1,27 @@
-import React from "react";
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 
 const Upload = () => {
   const [imgFile, setImgFile] = useState([]); // 이미지 배열
-  const upload = useRef(); 
+  const upload = useRef();
 
   const imgUpload = () => {
     console.log(upload.current.files);
-    setImgFile(prev => [...prev, URL.createObjectURL(upload.current.files[0])]);
+    setImgFile((prev) => [...prev, URL.createObjectURL(upload.current.files[0])]);
+  };
+
+  const handleUpload = () => {
+    if (imgFile.length === 0) {
+      alert("No images appended."); // Display alert if no images are appended
+    } else {
+      // Perform the AI logic here
+      //start the AI logic
+      // ...
+    }
   };
 
   return (
     <>
-      {' '}
-      <h1>image upload</h1>
+      <h1>서재 이미지 업로드</h1>
       <input
         type="file"
         ref={upload}
@@ -27,18 +35,24 @@ const Upload = () => {
         }}
       />
       <h2>이미지 저장소</h2>
-      <div style={{ display: 'flex' }}>
-        {imgFile?.map((img, idx) => (
-          <div key={idx} style={{ margin: '20px', border: '1px solid black' }}>
-            <img
-              style={{ width: '200px', height: '200px ' }}
-              src={img}
-              alt="img"
-            />
-          </div>
-        ))}
-      </div>
+      {imgFile.length === 0 ? (
+        <p>No images appended.</p>
+      ) : (
+        <div style={{ display: 'flex' }}>
+          {imgFile?.map((img, idx) => (
+            <div key={idx} style={{ margin: '20px', border: '1px solid black' }}>
+              <img
+                style={{ width: '200px', height: '200px' }}
+                src={img}
+                alt="img"
+              />
+            </div>
+          ))}
+        </div>
+      )}
+      <button onClick={handleUpload}>서재 목록 만들기</button>
     </>
   );
 };
+
 export default Upload;

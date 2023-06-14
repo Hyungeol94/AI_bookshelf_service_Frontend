eb3fbfc265584e3924e3819574d7e43b0d52ec6f
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-14 11:51:35 +0900

    feat: result page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,8 +1,51 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import sample from "../assets/sample_book.json";
 
-export default function result(){
+function Card({ children }) {
     return (
-        <></>
-    )
+      <div className="card" style={{backgroundColor: 'gray', padding : '10px', width: '350px'}}>
+        {children}
+      </div>
+    );
+  }
+
+function BookRow({ book_info }) {
+  return (
+    <tr>
+      <td>{book_info.booktitle}</td>
+      <td>{book_info.price}</td>
+    </tr>
+  );
+}
+
+function BookTable({ books_info }) {
+  return (
+    <div style={{backgroundColor: 'white', padding : '10px', width: '300px'}}>
+      <table>
+        <thead>
+          <tr>
+            {/* <th>Title</th>
+            <th>Price</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {books_info.map((book_info) => (
+            <BookRow
+              key={book_info.id}
+              book_info={book_info}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default function Result() {
+  return (
+    <Card >
+        <BookTable books_info={sample} />;
+    </Card>
+    );
 }

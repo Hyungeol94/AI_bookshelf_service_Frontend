9b0dc27b47e5d602627ca5def7aad5a0aa0ed734
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-14 17:58:23 +0900

    style: fix display error

--- src/components/GetList.js
+++ src/components/GetList.js
@@ -1,31 +1,30 @@
 import React from "react";
 import BookList from "./BookList";
+import "../styles/GetList.css";
 
 const Book = (props) => (
-    <div>
-      <div>
-        <img src={props.image} 
-        alt={props.booktitle} 
-        width={80} 
-        height={80}
-        style = {{marginRight : "10px"}}
-        />
-      </div>
-      <h5>{props.booktitle}</h5>
+  <div>
+    <div className="book-image">
+      <img
+        src={props.image}
+        alt={props.booktitle}
+        style={{ width: "80px", height: "80px"}}
+      />
     </div>
-  );
-  
+    <h5>{props.booktitle}</h5>
+  </div>
+);
 
+  
 export default function getlist(books_info){ 
     return ([
       books_info.length,
-      <div style={{display : "flex", overflowX: "auto"}}>
+      <div className="book-container">
       {books_info.map((book_info) => (
       <Book
         key={book_info.id}
         image={book_info.image}
         booktitle={book_info.booktitle}
-        style = {{marginRight : "10px"}}
       />
     ))}
     </div>

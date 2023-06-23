e3a001c82e328a119e64308dfa281fde71c93332
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-23 11:27:47 +0900

    Refactor: make a separate BookSearchBar.js file

---
+++ src/components/Result/BookSearchBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const BookSearchBar = (props) => {
+    const {inputRef, searchValue, setSearchValue} = props
+
+    const handleClick = () =>{
+        if (inputRef.current.value!==''){    
+         setSearchValue(inputRef.current.value)
+      }
+      }
+      
+      const handleOnKeyPress = (e) => {
+        if (e.key === "Enter") {
+          if (inputRef.current.value!==''){    
+            setSearchValue(inputRef.current.value)
+          }
+        }
+      };
+      
+      const handleFocus = () => {
+        inputRef.current.value = searchValue;
+      }
+    
+    return (
+    <div class="input-group mb-3" >
+            <input 
+                ref ={inputRef}
+                defaultValue={searchValue}
+                placeholder= {searchValue}
+                onFocus={handleFocus} // Preserve the defaultValue when the input is focused
+                className = "searchBar"           
+                class="form-control"                         
+                aria-label="book search" 
+                aria-describedby="button-addon2" 
+                type="text"          
+                onKeyDown={handleOnKeyPress}            
+                style={{color: "black"}}/>
+                        
+            <a class="nav-link d-none d-lg-block btn btn-default" onClick={handleClick}>
+                <i class="tim-icons icon-zoom-split"></i>
+            </a>
+    </div>
+    )
+}
+
+export default BookSearchBar;
--- src/components/Result/BookSearchView.js
+++ src/components/Result/BookSearchView.js
@@ -1,31 +1,16 @@
 import BookSearchResultRow from "./BookSearchResultRow"
+import BookSearchBar from "./BookSearchBar"
 import "../../styles/Result.css";
 import {useRef, useState, useEffect} from 'react'
+import React from 'react';
+import { Book } from "@mui/icons-material";
 
-export default function BookSearchView(props) { 
+const BookSearchView = (props) => { 
     const {setSelectedBookInfo, searchValue, setSearchValue, isLoading, data} = props
     const inputRef = useRef(searchValue);
 
-  
     let num = 1;
-    const handleClick = () =>{
-      if (inputRef.current.value!==''){    
-       setSearchValue(inputRef.current.value)
-    }
-    }
     
-    const handleOnKeyPress = (e) => {
-      if (e.key === "Enter") {
-        if (inputRef.current.value!==''){    
-          setSearchValue(inputRef.current.value)
-        }
-      }
-    };
-    
-    const handleFocus = () => {
-      inputRef.current.value = searchValue;
-    }
-
     useEffect(() => {
       inputRef.current.value = searchValue
     }, [searchValue]);
@@ -35,24 +20,11 @@ export default function BookSearchView(props) {
       <div style={{backgroundColor: 'white', padding : '10px', width: '300px', height:'450px'}}>
         <h3 style={{color:'black'}}>도서 검색 결과</h3>      
         {/* <div>검색창 넣기</div>  */}        
-        <div class="input-group mb-3" >
-          <input 
-            ref ={inputRef}
-            defaultValue={searchValue}
-            placeholder= {searchValue}
-            onFocus={handleFocus} // Preserve the defaultValue when the input is focused
-            className = "searchBar"           
-            class="form-control"                         
-            aria-label="book search" 
-            aria-describedby="button-addon2" 
-            type="text"          
-            onKeyDown={handleOnKeyPress}            
-            style={{color: "black"}}/>
-                      
-          <a class="nav-link d-none d-lg-block btn btn-default" onClick={handleClick}>
-            <i class="tim-icons icon-zoom-split"></i>
-          </a>
-        </div>
+        <BookSearchBar
+          inputRef = {inputRef}
+          searchValue = {searchValue}
+          setSearchValue = {setSearchValue}
+        />
         <div>
             {isLoading ? (
             <h3 style={{color: 'black'}}>로딩중..</h3>
@@ -78,4 +50,6 @@ export default function BookSearchView(props) {
       </div>
     );
   }
+
+  export default BookSearchView; 
 

fe5211ca1139f7fa6db2eb950cc49227b3d1a238
swcheon <swwe1000@gmail.com>
2023-06-26 14:16:51 +0900

    Design: about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -34,15 +34,17 @@ const AboutUs = () => {
   return(
     <div className="wrapper">
       <IndexNavbar />
-      <div style={{ "margin-top": "9%", "margin-left": "10%", "margin-bottom": "7%", display:"flex", alignItems: "center" }} className="about-us">
+      <div style={{ "margin-top": "12%", "margin-left": "16%", "margin-bottom": "7%", display:"flex", alignItems: "center" }} className="about-us">
         {/* <img src="https://aivle.kt.co.kr/app/file/view2/546" style={{width: "800px"}}/>  <br/> */}
-        <img src={require("assets/img/us.jpg")} style={{ width: "500px",  border: "5px solid rgb(220, 220, 220)", }} />
-        <div style={{ display: "flex", flexDirection: "column", width: "600px", "margin-left": "32px"}}>
-          <p className="title"> aivle 17조 </p>
-          <p> B냉C켜조 </p>
-          <h3>이 웹사이트는 KT AIVLE SCHOOL AI 개발자 교육 3기 과정 17조의 빅프로젝트 산출물입니다. </h3>
-          <h4> 조장: 김영빈 </h4>
-          <h4> 조원: 김덕주, 박재경, 신윤하, 이현걸, 천승우, 최서연 </h4>
+        <img src={require("assets/img/us.jpg")} style={{ width: "520px",  border: "5px solid rgb(220, 220, 220)", }} />
+        <div style={{ display: "flex", flexDirection: "column", width: "500px", "margin-left": "32px"}}>
+          <h1 style={{ "font-size":"60px" }}> <strong> AIVLE 17조 </strong></h1>
+          <h3>이 웹사이트는 KT AIVLE SCHOOL 3기 AI 개발자 교육과정 5반 17조의 빅프로젝트 산출물입니다.</h3>
+          <h4> 조장: 김영빈 <br/> 조원: 김덕주, 박재경, 신윤하, 이현걸, 천승우, 최서연 </h4>
+          <h4> </h4>
+          
+          <h3> Contact Us </h3>
+          <h4> 연락처: 000-000-0000 <br/> 이메일: email@gmail.com <br/> 주소: 경기 성남시 분당구 불정로 90</h4>
         </div>
       </div>
       <img alt="..." className="path" src={require("assets/img/path1.png")}

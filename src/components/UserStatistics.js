cc03ab48cebe5c8730fc245d5bc4b94daff713b0
Seoyeon Choi <westkite720@gmail.com>
2023-06-27 15:22:24 +0900

    Design: user dashboard

--- src/components/UserStatistics.js
+++ src/components/UserStatistics.js
@@ -1,41 +1,54 @@
-// import React, { useEffect, useState } from 'react';
-// import stat from '../assets/sample_statistics.json';
-
-// const StatShow = () => {
-//   const [bookStatsData, setBookStatsData] = useState(null);
-
-//   useEffect(() => {
-//     // JSON 파일 데이터를 불러옵니다.
-//     setBookStatsData(stat);
-//   }, []);
-
-//   if (!bookStatsData) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>Book Stats</h1>
-//       <div>
-//         <img src={bookStatsData.image} alt="Book Stats" style = {{
-//         width: "300px"}}/>
-//       </div>
-//       <h3>title: {bookStatsData.statsTitle}</h3>
-//       <p>info: {bookStatsData.statsInfo}</p>
-//       <p>Favorite Genre: {bookStatsData.favoriteGenre}</p>
-//       <p>Books Read: {bookStatsData.booksRead}</p>
-//     </div>
-//   );
-// };
-
-// export default StatShow;
-
-
 import React, { useEffect, useState } from 'react';
 import stat from '../assets/sample_statistics.json';
-import { Modal } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody  } from "reactstrap";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-const StatShow = () => {
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data_chart = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(255, 159, 64, 0.5)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 0,
+    },
+  ],
+};
+
+
+const StatShow = (props) => {
+
+  const {
+    email,
+    name,
+    user_id,
+    nickname,
+    user_bookshelf,
+    user_like_book,
+    user_cart,
+    user_interest,
+    user_type,
+  } = props?.data;
+  console.log(props?.data);
+
   const [bookStatsData, setBookStatsData] = useState(null);
   let [modalIsOpen, setModalIsOpen] = useState(false); // 모달 변수
 
@@ -58,25 +71,105 @@ const StatShow = () => {
 
   return (
     <div>
-      <button onClick={openModal}> 통계정보 확인하기 </button>
+      <Button 
+        style={{ marginTop: "0px" }}
+        className="btn-round"
+        color="primary"
+        size="sm"
+        onClick={openModal}> 나의 독서폴리오 확인하기 </Button>
+
       <Modal isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="File Modal"
-        className="modal-box" style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, width: "600px", height: "200px" }}>
+        size="xl"
+        className="modal-box" style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, width: "10000px", height: "200px"}}>
+
+        <ModalHeader className="justify-content-center" >
+          Bookpolio
+          <Button
+            className="btn-round btn-neutral btn-icon"
+            onClick={closeModal}> 
+            <i className="tim-icons icon-simple-remove" />
+          </Button>
+        </ModalHeader>
+
         <div style={{margin: "15px"}}>
-          <button onClick={closeModal}> 닫기 </button>
-          <h1 style={{color: "black"}}>Book Stats</h1>
-          <div>
-            <img src={bookStatsData.image} alt="Book Stats" style={{width: "300px"}} />
+          <div className='subject'
+            style={{marginTop:'20px', marginBottom:'30px'}}>
+            <h2 style={{color: "black", textAlign:'center', fontWeight:'bold', fontSize:'3em'}}>{nickname}님의 독서폴리오</h2>
+            <h4 style={{color: "black", textAlign:'center', fontWeight:'normal', }}>{nickname}님의 Mybrary를 바탕으로 어떤 책 취향이 있으신지 찾아봤어요!</h4>
           </div>
-          <h3 style={{color: "black"}}>title: {bookStatsData.statsTitle}</h3>
-          <p style={{color: "black"}}>
-            info: {bookStatsData.statsInfo} <br/>
-            Favorite Genre: {bookStatsData.favoriteGenre} <br/>
-            Books Read: {bookStatsData.booksRead}
-            </p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: '10px',
+              marginBottom: '10px',
+              paddingLeft: '0.8em',
+              paddingRight: '0.8em',
+            }}
+          >
+              
+              <div className='block_section1&2'
+                style={{
+                  display: 'block',
+                  marginLeft:'1em'
+                }}>
+
+                <div className='section1'
+                  style={{
+                    width: '40vw',
+                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                    marginBottom: '20px',
+                    borderRadius: '20px 20px 20px 20px' }}>
+                  <div style={{ paddingLeft: '3em', paddingTop: '2em', paddingBottom: '1em' }}>
+                    <div>
+                      <h3 style={{ color: '#000000', fontWeight:'bold' }}>{nickname}님의 독서 취향</h3>
+                    </div>
+                    <div>
+                      <h4 style={{ color: '#000000' }}>나의 독서 유형: 미래의 코난</h4>
+                      <h4 style={{ color: '#000000' }}>내가 좋아하는 장르: 탐정 소설</h4>
+                      <h4 style={{ color: '#000000' }}>내가 좋아하는 작가: 히가시노 게이고</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='section2_calc'
+                  style={{
+                    width: '40vw',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderRadius: '20px 20px 20px 20px' }}>
+                  <div style={{ paddingLeft: '3em', paddingTop: '2em', paddingBottom: '1em' }}>
+                    <div>
+                      <h3 style={{ color: '#000000', fontWeight:'bold' }}>{nickname}님의 독서 취향</h3>
+                    </div>
+                    <div>
+                      <h4 style={{ color: '#000000' }}>나의 독서 유형: 미래의 코난</h4>
+                      <h4 style={{ color: '#000000' }}>내가 좋아하는 장르: 탐정 소설</h4>
+                      <h4 style={{ color: '#000000' }}>내가 좋아하는 작가: 히가시노 게이고</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='section3_chart'
+                style={{
+                  width: '50%',
+                  display: 'flex',
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}>
+                <Pie data={data_chart}
+                  style={{width:'50%'}} />
+              </div>
+
+              </div>
+
         </div>
+
       </Modal>
+
     </div>
   );
 };

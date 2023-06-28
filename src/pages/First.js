5dc7aeff083caf6bad92be453bf7c4ff39e6b0a2
swcheon <swwe1000@gmail.com>
2023-06-28 10:39:15 +0900

    Design: descriptions on First page

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // reactstrap components
 import { Container, Button } from "reactstrap";
 import { animateScroll, scroller, Link } from "react-scroll";
@@ -25,79 +25,135 @@ export default () => {
     });
   };
 
+  const ToTheTop = () => {
+  // 토글 여부를 결정하는 state 선언
+  const [toggleBtn, setToggleBtn] = useState(true);
+
+  // window 객체에서 scrollY 값을 받아옴
+  // 어느정도 스크롤이 된건지 판단 후, 토글 여부 결정
+  const handleScroll = () => {
+    const { scrollY } = window;
+
+    scrollY > 200 ? setToggleBtn(true) : setToggleBtn(false);
+  };
+
+  // scroll 이벤트 발생 시 이를 감지하고 handleScroll 함수를 실행
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // 버튼 클릭 시 스크롤을 맨 위로 올려주는 함수
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // 토글 여부 state에 따라 버튼을 보여주거나 감추게 만듦
+  return toggleBtn ? (
+    <div onClick={goToTop}>
+		// ... //
+    </div>
+  ) : null;
+}
+
   return(
     <div className="wrapper">
-    <div className="page-header header-filter">
-      <div className="squares square1" />
-      <div className="squares square2" />
-      <div className="squares square3" />
-      <div className="squares square4" />
-      <div className="squares square5" />
-      <div className="squares square6" />
-      <div className="squares square7" />
-      
-      <Container>
-        <div className="content-center brand">
-          <h1 className="h1-seo">Book is On & On</h1>
-          <h2 className="d-none d-sm-block">
-            인공지능으로 만드는 나만의 디지털 서재
-          </h2>
-          <Link to="section1" smooth={true} duration={1000}>
-            <Button color="info" size="lg">
-            알아보기
-          </Button>
-          </Link>
-        </div>
-      </Container>
-    </div>
-    <div className="main">
-      <div className="section section-basic" id="section1">
-          <Container>
-          <div className="squares square3" />
-          <div className="squares square5" />
-          {/* <div className="squares square7" /> */}
-          <div style={{height:"100vh", "margin-top": "20vh"}}>
-            <h1>Section 1</h1>
-            <div className="content-center brand">
-          <h1 className="h1-seo">Book is On & On</h1>
-          <img src="https://i.ytimg.com/vi/gwHQLBUStO4/maxresdefault.jpg" className="squares square8" />
-          <h2 className="d-none d-sm-block">
-            인공지능으로 만드는 나만의 디지털 서재
-          </h2>
-          
-          <Link to="section2" smooth={true} duration={1000}>
-            <Button color="info" size="lg">
-            다음
-          </Button>
-          </Link>
+      <div className="page-header header-filter">
+        <div className="squares square1" />
+        <div className="squares square2" />
+        <div className="squares square3" />
+        <div className="squares square4" />
+        <div className="squares square5" />
+        <div className="squares square6" />
+        <div className="squares square7" />
+        
+        <Container>
+          <div className="content-center brand">
+            <h1 className="h1-seo">Book is On & On</h1>
+            <h2 className="d-none d-sm-block">
+              인공지능으로 만드는 나만의 디지털 서재
+            </h2>
+            <Link to="section1" smooth={true} duration={1000}>
+              <Button color="info" size="lg">
+                알아보기
+              </Button>
+            </Link>
           </div>
+        </Container>
+      </div>
+      <div className="main">
+        <div className="section section-basic" id="section1">
+          <Container>
+            <div className="squares square3" />
+            <div className="squares square5" />
+            {/* <div className="squares square7" /> */}
+            <div style={{ height: "100vh", "margin-top": "20vh" }}>
+              <h1>Book is On & On</h1>
+              <div className="content-center brand">
+                <h2 className="h1-seo"> 첫번째, 오프라인 서재의 온라인화 </h2>
+                <img src={require("assets/img/mybookshelf.jpg")} className="floatimages image1" style={{ border: "2px solid white" }} />
+                <h2 className="d-none d-sm-block">
+                오프라인 서재 사진을 온라인에 등록하고 관리할 수 있어요.
+                </h2>
 
-        </div>
+                <Link to="section2" smooth={true} duration={1000}>
+                  <Button color="info" size="lg">
+                    다음
+                  </Button>
+                </Link>
+              </div>
+
+            </div>
           </Container>
         </div>
         <div className="section section-basic" id="section2">
-          {/* <div className="squares square3" /> */}
-          {/* <div className="squares square5" /> */}
           <div className="squares square7" />
           <Container>
-          <div style={{height:"100vh", "margin-top": "20vh"}}>
-            <h1>Section 2</h1>
-            <div className="content-center brand">
-          <h1 className="h1-seo">Book is On & On</h1>
-          <h2 className="d-none d-sm-block">
-            인공지능으로 만드는 나만의 디지털 서재
-          </h2>
-          <Button color="info" size="lg" href="/login">
-                회원가입하고 서비스 시작하기
-              </Button>
-        </div>
-        </div>
+            <div style={{ height: "100vh", "margin-top": "20vh" }}>
+              <h1><strong> Book is On & On </strong></h1>
+              <div className="content-center brand">
+                <h1 className="h1-seo"><strong> 두번째, 도서 관리 기능 </strong></h1>
+                <img src={require("assets/img/bookmodal.jpg")} className="floatimages image2" style={{ border: "2px solid white" }} />
+                <h2 className="d-none d-sm-block">
+                등록된 온라인 서재를 기반으로 나의 책 취향을 확인할 수 있어요.
+                </h2>
+                <Link to="section3" smooth={true} duration={1000}>
+                  <Button color="info" size="lg">
+                    다음
+                  </Button>
+                </Link>
+              </div>
+            </div>
           </Container>
         </div>
-    <div className="main"></div>
-    <Footer />
+
+        <div className="section section-basic" id="section3">
+          <div className="squares square7" />
+          <Container>
+            <div style={{ height: "100vh", "margin-top": "20vh" }}>
+              <h1><strong> Book is On & On </strong></h1>
+              <div className="content-center brand">
+                <h1 className="h1-seo"><strong>  세번째, 도서 추천 기능  </strong></h1>
+                <img src={require("assets/img/bookmodal.jpg")} className="floatimages image2" style={{ border: "2px solid white" }} />
+                <h2 className="d-none d-sm-block">
+                나의 책들을 바탕으로 새로운 책들을 추천해줘요.
+                </h2>
+                <Button color="info" size="lg" href="/login">
+                  회원가입하고 서비스 시작하기
+                </Button>
+              </div>
+            </div>
+          </Container>
+        </div>
+
+        <div className="main"></div>
+        <Footer />
+      </div>
     </div>
-    </div>
+    // 이미지 애니메이션 설정은 assets/scss/blk-design-system-react/custom에 있음!
   )
   
 };

fe77848a853ffecdb811b8c25dba8cbb8581bce9
Seoyeon Choi <westkite720@gmail.com>
2023-06-28 13:48:50 +0900

    sapzil

---
+++ src/components/Bookslider.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import stat from "../assets/sample_statistics.json";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import * as api from "../services/api";
+
+const book_slider = {}
+
+export default book_slider;

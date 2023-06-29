3236beec4e933797c58464fa89f21d0ca4598c74
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-29 10:46:59 +0900

    Feat: saveToMyBookShelf

--- src/components/Result/BookTableView.js
+++ src/components/Result/BookTableView.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Button} from "reactstrap";
 import BookTable from './BookTable'
 import "../../styles/Result.css";
+import * as api from "../../services/api";
+import {
+  // Link, Route, Routes,
+  useNavigate,
+} from "react-router-dom";
 
 const BookTableView = (props) => {
     const {booksInfo, setSelectedBookInfo, deleteFromBookList, selectedBookRowInfo, setSelectedBookRowInfo, searchValue, setSearchValue, onSearch, bookInfoAPI} = props  
-
+    const navigate = useNavigate();
     const searchBookInfo = async (bookInfo) => {
       console.log('searched')
       const fetchedData = await bookInfoAPI(bookInfo.title, 1);
@@ -17,26 +22,27 @@ const BookTableView = (props) => {
     }
 
     const saveToMyBookShelf = (e) => {
-      // console.log('clicked')
-      // const newBooksInfo = []
-      // const promises = booksInfo.map((bookInfo) => {
-      //   if (bookInfo.isbn === undefined) {
-      //     return searchBookInfo(bookInfo);
-      //   } else {
-      //     return Promise.resolve(bookInfo);          
-      //   }
-      // });
+      console.log('clicked')
+      const newBooksInfo = []
+      const promises = booksInfo.map((bookInfo) => {
+        if (bookInfo.isbn === undefined) {
+          return searchBookInfo(bookInfo);
+        } else {
+          return Promise.resolve(bookInfo);          
+        }
+      });
 
-      // Promise.all(promises).then((results) => {
-      //   newBooksInfo.push(...results);
-      //   console.log([...newBooksInfo]);
-      //   newBooksInfo.forEach((bookInfo) => {
-      //     if (bookInfo !=={}){
-      //       //save this bookInfo to myBookShelf DB  
-
-      //     }
-      //   })
-      // });      
+      Promise.all(promises).then((results) => {
+        newBooksInfo.push(...results);
+        console.log([...newBooksInfo]);
+        newBooksInfo.forEach(async (bookInfo) => {
+          if (Object.keys(bookInfo).length !==0){
+            //save this bookInfo to myBookShelf DB  
+            await api.addbookshelf(bookInfo)
+          }
+        })
+        navigate('/bookshelf', { replace: true })
+      });      
     }
 
     return (

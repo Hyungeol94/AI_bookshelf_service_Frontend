4970209132963dedf5504edfbc6a4e50ee312923
Hyungeol94 <ekfrhkquf12@naver.com>
2023-06-30 16:59:28 +0900

    Style: align buttons to the center

--- src/components/Result/BookTable.js
+++ src/components/Result/BookTable.js
@@ -2,27 +2,29 @@ import React from 'react';
 import BookRow from './BookRow';
 
 const BookTable = (props) => {
-    const {booksInfo, setSelectedBookInfo, selectedBookRowInfo, setSelectedBookRowInfo, deleteFromBookList, searchValue, setSearchValue, onSearch} = props        
+    const {booksInfo, 
+          setSelectedBookInfo, 
+          selectedBookRowInfo, 
+          setSelectedBookRowInfo, 
+          deleteFromBookList, 
+          searchValue, 
+          setSearchValue, 
+          onSearch} = props        
 
     return (        
         <table className="bookRowTable">
-          <thead>
-            <tr>
-              {/* <th>Title</th>
-              <th>Price</th> */}
-            </tr>
-          </thead>
+          
           <tbody >
-            {booksInfo.map((bookInfo) => (
-              <BookRow key={bookInfo.id} 
-              bookInfo={bookInfo} 
-              setSelectedBookInfo={setSelectedBookInfo}
-              selectedBookRowInfo={selectedBookRowInfo}
-              setSelectedBookRowInfo = {setSelectedBookRowInfo}
-              deleteFromBookList = {deleteFromBookList}      
-              searchValue = {searchValue}      
-              setSearchValue={setSearchValue}
-              onSearch={onSearch}/>
+            {booksInfo.map((bookInfo) => (            
+                <BookRow key={bookInfo.id} 
+                bookInfo={bookInfo} 
+                setSelectedBookInfo={setSelectedBookInfo}
+                selectedBookRowInfo={selectedBookRowInfo}
+                setSelectedBookRowInfo = {setSelectedBookRowInfo}
+                deleteFromBookList = {deleteFromBookList}      
+                searchValue = {searchValue}      
+                setSearchValue={setSearchValue}
+                onSearch={onSearch}/>
             ))}
           </tbody>
         </table>
